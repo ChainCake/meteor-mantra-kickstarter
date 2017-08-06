c299accbf30d5d260e3f6d25754a660ba531a47b
Borodin <borodin@amazon.com>
2017-08-05 21:42:22 -0700

    the contract compiles

--- private/icing-oz-workspace/contracts/Job.sol
+++ private/icing-oz-workspace/contracts/Job.sol
@@ -7,53 +7,71 @@ contract Job {
     uint public payment;
 
     uint public quota;
-    uint public hired;
+    address[] hired;
 
-    unit maxApplicants; // > than quota to make sure the creator has enough to choose from
+    uint maxApplicants; // > than quota to make sure the creator has enough to choose from
+    uint appliedCount;
 
     // key is an applicant, value is a rating. 0 by default (i.e. not reviewed), [1,5] assigned EOD
     mapping (address => uint) applicants; 
 
-    struct Review {
-        address worker;
-        uint rating;
-    }
-
     // Worker applies to the job
     function apply() public returns (bool success) {
-        if (maxApplicants >= applicants.length) {return false;} // TODO: a bug, use throw instead
+        if (appliedCount >= maxApplicants) {throw;}
 
-        applicants[msg.sender]= 0;
+        applicants[msg.sender] = 0;
+        appliedCount++;
         return true;
     }
 
-    function hire(address[] workers) public returns (address[] hired) {
+    function hire(address[] workers) public returns (bool[]) {
         if (msg.sender != owner) {throw;}
 
+        bool[] memory hiredStatus = new bool[](workers.length);
         for (var i=0; i < workers.length; i++) {
             // assumption that a worker has exactly one coin, that is transferred to job creator (??)
             // at the time of hiring. It should be released after work
             // That create a lot of potential problems and should be refactored, but I want to go faster
-            if (worker[i].balance > 1) {
+            hiredStatus[i] = false;
+            if (workers[i].balance > 1) {
                 // at least, we should charge the owner more for creating the job to motivate him to write reviews and return coins to workers
-                worker[i].transfer(1);
-                hired.push(worker[i]);
+                workers[i].transfer(1);
+                hiredStatus[i] = true;
+
+                hired.push(workers[i]);
             }
         }
     }
 
-    function review(Review[] reviews) public {
+    // TODO: redesign the API. A struct should be used, however, I got trouble with it
+    // going with this to save time. Yeah, we can't use a mapping as param
+    function review(address[] workers, uint[] rating) public returns (bool) {
         if (msg.sender != owner) {throw;}            
 
-        for(var i=0; i < reviews.length; i++) {
-            applicants[reviews[i].worker] = reviews[i].rating;
-            reviews[i].worker.send(1);
+        assert (workers.length == rating.length);
+
+        for (var i = 0; i < workers.length; i++) {
+            applicants[workers[i]] = rating[i];
+            // TODO: Bug, could send > 1 time
+            // Also, shouldn't throw I think
+            if (!workers[i].send(1)) {
+                throw;
+            }
         }
+
+        for (i = 0; i < hired.length; i++) {
+            if (applicants[hired[i]] <= 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     function destroy() { // so funds not locked in contract forever
         if (msg.sender == owner) {
             suicide(owner); // send funds to the creator
         }
+        // TODO: refund workers
     }
 }
